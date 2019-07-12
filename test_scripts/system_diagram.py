13ba82ffe70424b3fb526dca194b52ef89e20ccd
Karthack <karthik.subramanya@iitgn.ac.in>
2019-07-11 17:09:44 -0700

    Add files via upload

--- test_scripts/system_diagram.py
+++ test_scripts/system_diagram.py
@@ -1,21 +1,21 @@
 # filename: system_dyanmics.py
 #
-# by: Abhay Gupta
-# date: 05/08/19
+# by: Abhay Gupta & Karthik Subramanya Karvaje
+# date: 05/18/19
 #
 # description: form plots of system dynamics
-
 
 import time
 from time import sleep
 import hebi
 import numpy as np
 import math as m
+import csv
 
 def main():
     '''
     description: step function of system
-
+    position of actuator shaft follows a sine wave path
     parameters: none
     returns: none
     '''
@@ -29,19 +29,25 @@ def main():
     group_feedback = hebi.GroupFeedback(group.size)
 
     group.feedback_frequency = 200.0    # This effectively limits the loop below to 200Hz
+    #group.feedback_frequency = 50.0    # This effectively limits the loop below to 200Hz
 
     ## Open-loop controller ----------------------------------------------------------------------
 
-    w = -1.0*m.pi*2.0 # frequency
-    t0, dt, tf = 0.0, 0.1, 2.0 # initial time & step size
+    w = -1.0*m.pi*4.0 # frequency
+
+    #setting initial positipon to 0
+    group_command.position = 0
+    time.sleep(2)
+    #t0, dt, tf = 0.0, 0.1, 2.0 # initial time & step size
+    t0, dt, tf = 0.0, 0.04, 15.0 # initial time & step size
     time_range = np.arange(t0,tf,dt)
     group.command_lifetime = dt # set length of time of command (ms)
 
     # Initialize all array
-    pos, vel, eff = [], [], []
+    pos, vel, defl, eff, pwm = [], [], [], [], []
     stop_loop = 0
     t = t0
-
+    
     # First restart poistion
     while(0):
         group.command_lifetime = 200.0 # set length of time of command (ms)
@@ -62,47 +68,80 @@ def main():
         
         group_feedback = group.get_next_feedback(reuse_fbk=group_feedback)
         w_t = np.multiply(w,t)
-        group_command.velocity = 0.5*np.sin(w_t)
+        group_command.velocity = 0.5*np.cos(w_t)
         group.send_command(group_command)
 
         print('input effort:', group_command.effort)
 
         positions = group_feedback.position
         velocity = group_feedback.velocity
+        deflection = group_feedback.deflection
         effort = group_feedback.effort
+        pwm_command = group_feedback.pwm_command
         print('Position Feedback:\n{0}'.format(positions))
 
         pos = np.append(pos, positions)
         vel = np.append(vel, velocity)
+        defl = np.append(defl, deflection)
         eff = np.append(eff, effort)
+        pwm = np.append(pwm, pwm_command)
 
+        '''with open('motor.csv', 'w') as csvfile:
+            fieldnames = ['pos', 'vel', 'eff']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerow({'pos': pos, 'vel': vel, 'eff':eff})'''
+            
+        row = [ pos, vel, defl, eff, pwm]
+        #print ('row:', row)
         sleep(dt)
         t = t + dt
-
+        
     end_time = time.time()
 
     print('Time: ', end_time-start_time)
+    print ('pos', pos)
+    print ('vel', vel)
+    print ('deflection', defl)
+    print ('eff', eff)
+    print ('pwm', pwm)
 
+    
+    with open('motor.csv', 'w') as csvfile:
+        fieldnames = ['pos', 'vel','defl', 'eff', 'pwm']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerow({'pos': pos, 'vel': vel, 'defl':defl, 'eff':eff, 'pwm':pwm})
+    
     group_command.velocity = np.nan #0.001*np.cos(w_t)h
     group.send_command(group_command)
 
     import matplotlib.pyplot as plt
 
-    plt.subplot(311)
+#    row = []
+#I am also importing the deflection data to look for any correlations.
+
+    plt.subplot(511)
     plt.plot(pos)
     plt.ylabel('position')
-    plt.subplot(312)
+    plt.subplot(512)
     plt.plot(vel)
     plt.ylabel('velocity')
-    plt.subplot(313)
+    plt.subplot(513)
     plt.plot(eff)
     plt.ylabel('torque (N m)')
+    plt.subplot(514)
+    plt.plot(defl)
+    plt.ylabel('deflection')
+    plt.subplot(515)
+    plt.plot(pwm)
+    plt.ylabel('pwm')
     plt.xlabel('time')
     plt.show()
 
 
 if __name__ == '__main__':
     main()
-
-
 
